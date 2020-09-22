6dd79592e7de3c07af9674d3fa181301a6f001c5
João Carlos Belmiro Conçalves <14908@adm.bancoamazonia.sa>
2020-09-22 10:42:57 -0300

    correçao alterar usuario

--- src/services/usuario.js
+++ src/services/usuario.js
@@ -2,7 +2,7 @@ import { http } from './config.js'
 const user = {
   salvar: (usuario) => http.post('/usuario/criar', usuario),
   filtro: (tag) => http.get('/usuario/tag', { params: { tag: tag } }),
-  alterar: (usuario) => http.put('/usuario/alterar', usuario),
+  alterar: (usuario) => http.post('/usuario/alterar', usuario),
   deletar: (id) => http.delete(`/usuario/deletar/${id}`),
   buscarPorId: (id) => http.get(`/usuario/buscar/${id}`)
 }
